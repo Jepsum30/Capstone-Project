10ce09fcbdbd7839f49e8bd9b5f4c8c69b00bd1a
Jepsum30 <joshua.gibson.27@gmail.com>
2026-05-29 20:02:25 -0400

    Fixed all the html files to add bootstrap 5 styling and global nav bar.

--- obj/Debug/net10.0/AdjusterOptimizerAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/AdjusterOptimizerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb65e63216cddb149c1421ec62623e0d7739f08f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2e8ea0f6f04b8f4d024e0680482970e3f313b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
