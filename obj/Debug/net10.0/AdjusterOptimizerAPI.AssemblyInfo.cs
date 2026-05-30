ca1f6df9f283508e0ecc0346ae0020a1c51cddd8
Jepsum30 <joshua.gibson.27@gmail.com>
2026-05-29 20:29:43 -0400

    add snl file

--- obj/Debug/net10.0/AdjusterOptimizerAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/AdjusterOptimizerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2e8ea0f6f04b8f4d024e0680482970e3f313b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ce09fcbdbd7839f49e8bd9b5f4c8c69b00bd1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdjusterOptimizerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
